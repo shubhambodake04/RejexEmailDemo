3af0aa8b2ecb27715e38cb321094cae310b614e9
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-24 19:25:15 +0530

    Add project files.

---
+++ RejexPatternDemo/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RejexEmailDemo
+
+{
+    class Pattern
+    {
+        string regex = "^[a-z0-9A-Z]+([.#@_][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
+
+        public bool Check(string input)
+        {
+            return Regex.IsMatch(input, regex);
+        }
+    }
+}
---
+++ RejexPatternDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RejexEmailDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Email Pattern Check Problem");
+            Pattern p = new Pattern();
+            bool result = p.Check("abc-100@abc.net,");
+            Console.WriteLine(result);
+        }
+    }
+}
